5fa69e99b821034c9675b8f6b82ebe85d257343f
Ahmed Mahmoud Sanad <sanada927@gmail.com>
2023-11-23 22:02:36 +0200

    Update Sum_without_highest_and_lowest_number.js

--- Sum_without_highest_and_lowest_number.js
+++ Sum_without_highest_and_lowest_number.js
@@ -1,6 +1,7 @@
 // Problem Link
 // https://www.codewars.com/kata/576b93db1129fcf2200001e6/train/javascript
 
+// First Solution:
 function sumArray(array) {
   if (!Array.isArray(array) || array.length < 3) return 0;
   else {
@@ -8,6 +9,17 @@ function sumArray(array) {
     var maxVal = Math.max(...array);
     var SumAll = array.reduce((acc, curr) => acc + curr, 0);
     return SumAll - minVal - maxVal;
+  }
+}
+
+// Second Solution:
+function sumArray(array) {
+  if (array == null) return 0;
+  else {
+    return array
+      .sort((a, b) => a - b)
+      .slice(1, -1)
+      .reduce((container, currVal) => container + currVal, 0);
   }
 }
 
